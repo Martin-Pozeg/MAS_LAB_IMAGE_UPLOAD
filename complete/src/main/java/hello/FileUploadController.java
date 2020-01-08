8da02f6eaa849f552d6cfe5f7b5b2cbc96705355
unknown <martin.pozeg@gmail.com>
2020-01-08 14:16:34 +0100

    dodan index.html

--- complete/src/main/java/hello/FileUploadController.java
+++ complete/src/main/java/hello/FileUploadController.java
@@ -9,12 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -30,6 +25,17 @@ public class FileUploadController {
 	@Autowired
 	public FileUploadController(StorageService storageService) {
 		this.storageService = storageService;
+	}
+
+
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String index(Model model){
+		return "index";
+	}
+
+	@RequestMapping(value="/test2", method=RequestMethod.GET)
+	public String test2(Model model){
+		return "test2";
 	}
 
 	@GetMapping("/")
